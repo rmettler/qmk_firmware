f871fb8e8f848197593aa197b6f0d3bb0107ebf7
Roger Mettler <roger@roger-mettler.ch>
2022-02-24 18:17:49 +0100

    move per key tap-hold config to separate file

--- keyboards/moonlander/keymaps/rome/keymap.c
+++ keyboards/moonlander/keymaps/rome/keymap.c
@@ -1,4 +1,5 @@
 #include "diahold.h"
+#include "taphold_config.h"
 
 #include <keyboards/moonlander/moonlander.h>
 
@@ -149,19 +150,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-typedef enum
-{
-    taphold_decision_mode_time_only,
-    taphold_decision_mode_other_key_tap,
-    taphold_decision_mode_other_key_press
-} taphold_decision_mode;
-
-typedef struct {
-    uint16_t              tapping_term;
-    bool                  repeat_primary;
-    taphold_decision_mode decision_mode;
-} taphold_config_t;
-
 static bool is_diahold(const uint16_t keycode) { return (keycode >= dh_a_ae && keycode <= dh_u_ue); }
 
 static bool is_alpha(const uint16_t keycode) { return (keycode >= KC_A && keycode <= KC_Z); }
@@ -182,35 +170,12 @@ static taphold_config_t map_taphold_config(const uint16_t keycode) {
         return (taphold_config_t){.tapping_term = 175, .repeat_primary = true, .decision_mode = taphold_decision_mode_other_key_press};
     }
     if (primary == KC_SPACE) {
-        return (taphold_config_t){.tapping_term = 150, .repeat_primary = true, .decision_mode = taphold_decision_mode_other_key_tap};
+        return (taphold_config_t){.tapping_term = 175, .repeat_primary = true, .decision_mode = taphold_decision_mode_other_key_tap};
     }
     if (primary == KC_TAB) {
-        return (taphold_config_t){.tapping_term = 150, .repeat_primary = false, .decision_mode = taphold_decision_mode_other_key_press};
+        return (taphold_config_t){.tapping_term = 175, .repeat_primary = false, .decision_mode = taphold_decision_mode_other_key_press};
     }
     return (taphold_config_t){.tapping_term = 500, .repeat_primary = false, .decision_mode = taphold_decision_mode_time_only};
-}
-
-uint16_t get_tapping_term(const uint16_t keycode, keyrecord_t *const record) {
-    const taphold_config_t config = map_taphold_config(keycode);
-    return config.tapping_term;
-}
-
-bool get_tapping_force_hold(const uint16_t keycode, keyrecord_t *const record) {
-    const taphold_config_t config = map_taphold_config(keycode);
-    dprintf("get_tapping_force_hold %d\n", !config.repeat_primary);
-    return !config.repeat_primary;
-}
-
-bool get_permissive_hold(const uint16_t keycode, keyrecord_t *const record) {
-    const taphold_config_t config = map_taphold_config(keycode);
-    dprintf("get_permissive_hold %d\n", config.decision_mode);
-    return config.decision_mode == taphold_decision_mode_other_key_tap || config.decision_mode == taphold_decision_mode_other_key_press;
-}
-
-bool get_hold_on_other_key_press(const uint16_t keycode, keyrecord_t *const record) {
-    const taphold_config_t config = map_taphold_config(keycode);
-    dprintf("get_hold_on_other_key_press %d\n", config.decision_mode);
-    return config.decision_mode == taphold_decision_mode_other_key_press;
 }
 
 static diahold_config_t map_diahold(const uint16_t keycode) {
